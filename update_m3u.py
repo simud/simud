a84e5b7f1eccf3b9741c7e56b3fb42fa067db10d
Simud <149002960+simud@users.noreply.github.com>
2025-04-12 19:43:27 +0200

    Update update_m3u.py

--- update_m3u.py
+++ update_m3u.py
@@ -4,7 +4,7 @@ import os
 from bs4 import BeautifulSoup
 
 # Single URL variable to control base_url, Origin, and Referer
-SITE_URL = "https://skystreaming.directory/"
+SITE_URL = "https://skystreaming.asia/"
 headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1",
     "Origin": SITE_URL.rstrip('/'),
