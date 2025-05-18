f8c03af6a9c51072e71a2e869a4c917c36a3d2dd
Simud <149002960+simud@users.noreply.github.com>
2025-05-18 19:59:01 +0200

    Update mpd.py

--- mpd.py
+++ mpd.py
@@ -6,7 +6,7 @@ def concatenate_m3u8():
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/sportstreaming_playlist.m3u8",
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/twitch_streams.m3u8",
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/Skympd.m3u8",
-        "https://raw.githubusercontent.com/simud/simud/refs/heads/main/simudflix.m3u8",
+        "https://raw.githubusercontent.com/simud/simud/refs/heads/main/simudflix2.m3u8",
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/vavoofunzionante.m3u8",
         "https://raw.githubusercontent.com/Brenders/Pluto-TV-Italia-M3U/refs/heads/main/PlutoItaly.m3u",
         "https://iptv-scraper-re.vercel.app/daddylive2/channels",
