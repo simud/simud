8a221d28cde46c7665189fd1fce33f45d26ef957
Simud <149002960+simud@users.noreply.github.com>
2025-05-21 12:28:24 +0200

    Update Daddy.py

--- Daddy.py
+++ Daddy.py
@@ -10,8 +10,7 @@ def concatenate_m3u8():
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/simudflix2.m3u8",
         "https://raw.githubusercontent.com/simud/simud/refs/heads/main/vavoofunzionante.m3u8",
         "https://iptv-scraper-re.vercel.app/daddylive2/channels",
-        "https://raw.githubusercontent.com/Brenders/Pluto-TV-Italia-M3U/refs/heads/main/PlutoItaly.m3u",
-        "https://raw.githubusercontent.com/simud/simud/refs/heads/main/itaevents3.m3u8"
+        "https://raw.githubusercontent.com/Brenders/Pluto-TV-Italia-M3U/refs/heads/main/PlutoItaly.m3u"
         # Aggiungi altri URL qui, fino a un massimo di 10
         # Esempio:
         # "https://example.com/playlist3.m3u8",
