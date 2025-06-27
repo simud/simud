5283529b57299f98f8432f1d25fafc8019f47a62
Simud <149002960+simud@users.noreply.github.com>
2025-06-27 17:48:19 +0200

    Update vavoofunzionante.py

--- vavoofunzionante.py
+++ vavoofunzionante.py
@@ -5,7 +5,7 @@ from github import Github
 from github import GithubException
 
 # URL della playlist
-url = "https://raw.githubusercontent.com/nzo66/TV/refs/heads/main/lista.m3u"
+url = "https://nzo66-tvproxy.hf.space/proxy?url=https://raw.githubusercontent.com/nzo66/TV/refs/heads/main/lista.m3u"
 
 # Gruppi desiderati
 gruppi_desiderati = ["Bambini", "Sport", "Film & Serie TV", "Documentari", "Eventi Live"]
